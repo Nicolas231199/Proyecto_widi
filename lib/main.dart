283aaab33a55034bb69ab50ed342fbb58981e6a0
FlutLab.io <>
2021-07-06 23:13:07 +0000

    Trabajando en el body

--- lib/main.dart
+++ lib/main.dart
@@ -12,4 +12,17 @@ void main() {
           child: Text("TAXI"),
         ),
       ),
-      
+      body: Container(
+        child: Center(
+          child: Text(
+            "Pagina principal",
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+        color: Colors.grey,
+      ),
+    ),
+  );
+
+  runApp(app);
+}
